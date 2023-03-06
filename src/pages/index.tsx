829320d1cf5143421ba95379655ab7d64f6117a3
이준용 <iiiuooyoo@gmail.com>
2023-03-06 16:33:15 +0900

    style : alert remove

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,174 +4,171 @@ import { MyCursor } from "@/components/MyCursor";
 import { useRouter } from "next/router";
 
 function Home() {
-  const router = useRouter();
+    const router = useRouter();
 
-  function register() {
-    if (AFRAME.components["sun-ref"]) {
-      return;
+    function register() {
+        if (AFRAME.components["sun-ref"]) {
+            return;
+        }
+
+        // 태양 이벤트
+        AFRAME.registerComponent("sun-ref", {
+            init: function () {
+                const data = this;
+                const el: HTMLElement = data.el;
+                const radius = parseInt(el.getAttribute("radius") ?? "0");
+
+                el.addEventListener("mouseenter", function () {
+                    el.setAttribute("radius", String(radius + 10));
+                });
+
+                el.addEventListener("mouseleave", function () {
+                    el.setAttribute("radius", String(radius));
+                });
+            },
+        });
+
+        // 지구 이벤트
+        AFRAME.registerComponent("earth-ref", {
+            init: function () {
+                const data = this;
+                const el: HTMLElement = data.el;
+                const radius = parseInt(el.getAttribute("radius") ?? "0");
+
+                el.addEventListener("mouseenter", function () {
+                    el.setAttribute("radius", String(radius + 10));
+                });
+
+                el.addEventListener("mouseleave", function () {
+                    el.setAttribute("radius", String(radius));
+                });
+
+                el.addEventListener("click", function () {
+                    router.push("/sky");
+                });
+
+                el.addEventListener("touchstart", function () {
+                    router.push("/sky");
+                });
+
+                el.addEventListener("touchend", function () {
+                    router.push("/sky");
+                });
+            },
+        });
     }
 
-    // 태양 이벤트
-    AFRAME.registerComponent("sun-ref", {
-      init: function () {
-        const data = this;
-        const el: HTMLElement = data.el;
-        const radius = parseInt(el.getAttribute("radius") ?? "0");
+    const [rendered, setRendered] = useState<boolean>(false);
 
-        el.addEventListener("mouseenter", function () {
-          el.setAttribute("radius", String(radius + 10));
-        });
+    useEffect(() => {
+        setRendered(true);
 
-        el.addEventListener("mouseleave", function () {
-          el.setAttribute("radius", String(radius));
-        });
-      },
-    });
+        if (typeof window !== "undefined") {
+            require("aframe"); // eslint-disable-line global-require
+            register();
+        }
+    }, [setRendered]);
 
-    // 지구 이벤트
-    AFRAME.registerComponent("earth-ref", {
-      init: function () {
-        const data = this;
-        const el: HTMLElement = data.el;
-        const radius = parseInt(el.getAttribute("radius") ?? "0");
-
-        el.addEventListener("mouseenter", function () {
-          el.setAttribute("radius", String(radius + 10));
-        });
-
-        el.addEventListener("mouseleave", function () {
-          el.setAttribute("radius", String(radius));
-        });
-
-        el.addEventListener("click", function () {
-          alert("click");
-          router.push("/sky");
-        });
-
-        el.addEventListener("touchstart", function () {
-          alert("touchStart");
-          router.push("/sky");
-        });
-
-        el.addEventListener("touchend", function () {
-          alert("touched");
-          router.push("/sky");
-        });
-      },
-    });
-  }
-
-  const [rendered, setRendered] = useState<boolean>(false);
-
-  useEffect(() => {
-    setRendered(true);
-
-    if (typeof window !== "undefined") {
-      require("aframe"); // eslint-disable-line global-require
-      register();
+    if (!rendered) {
+        return <>오아리 최고</>;
     }
-  }, [setRendered]);
 
-  if (!rendered) {
-    return <>오아리 최고</>;
-  }
+    return (
+        <Scene>
+            <Sky id="space-background" radius={10000} src="space.jpg" />
 
-  return (
-    <Scene>
-      <Sky id="space-background" radius={10000} src="space.jpg" />
+            <Entity
+                id="solar-system"
+                position={{
+                    x: 0,
+                    y: -300,
+                    z: -450,
+                }}
+                animation={{
+                    property: "rotation",
+                    to: "0 360 0",
+                    dur: 10000,
+                    loop: 100000,
+                    easing: "linear",
+                }}
+            >
+                <Sphere
+                    id="sun"
+                    radius={60}
+                    material={{
+                        src: "sun.jpg",
+                        shader: "flat",
+                    }}
+                    position={{
+                        x: 0,
+                        y: 0,
+                        z: 0,
+                    }}
+                    light={{
+                        type: "point",
+                        intensity: 2,
+                    }}
+                    sun-ref
+                />
 
-      <Entity
-        id="solar-system"
-        position={{
-          x: 0,
-          y: -300,
-          z: -450,
-        }}
-        animation={{
-          property: "rotation",
-          to: "0 360 0",
-          dur: 10000,
-          loop: 100000,
-          easing: "linear",
-        }}
-      >
-        <Sphere
-          id="sun"
-          radius={60}
-          material={{
-            src: "sun.jpg",
-            shader: "flat",
-          }}
-          position={{
-            x: 0,
-            y: 0,
-            z: 0,
-          }}
-          light={{
-            type: "point",
-            intensity: 2,
-          }}
-          sun-ref
-        />
+                <Sphere
+                    radius={20}
+                    material={{
+                        src: "earth.jpg",
+                    }}
+                    position={{
+                        x: 300,
+                        y: 0,
+                        z: 0,
+                    }}
+                    animation={{
+                        property: "rotation",
+                        to: "0 360 0",
+                        loop: 500000,
+                        dur: 5000,
+                        easing: "linear",
+                    }}
+                    earth-ref
+                >
+                    <Sphere
+                        radius={5}
+                        material={{
+                            src: "moon.jpg",
+                        }}
+                        position={{
+                            x: 40,
+                            y: 0,
+                            z: 0,
+                        }}
+                    ></Sphere>
+                </Sphere>
 
-        <Sphere
-          radius={20}
-          material={{
-            src: "earth.jpg",
-          }}
-          position={{
-            x: 300,
-            y: 0,
-            z: 0,
-          }}
-          animation={{
-            property: "rotation",
-            to: "0 360 0",
-            loop: 500000,
-            dur: 5000,
-            easing: "linear",
-          }}
-          earth-ref
-        >
-          <Sphere
-            radius={5}
-            material={{
-              src: "moon.jpg",
-            }}
-            position={{
-              x: 40,
-              y: 0,
-              z: 0,
-            }}
-          ></Sphere>
-        </Sphere>
+                <Box
+                    width={20}
+                    height={20}
+                    depth={20}
+                    material={{
+                        // src: "yong_sook.jpg",
+                        src: "ari.jpg",
+                    }}
+                    position={{
+                        x: -300,
+                        y: 0,
+                        z: 0,
+                    }}
+                    animation={{
+                        property: "rotation",
+                        to: "360 360 0",
+                        loop: 200000,
+                        dur: 2000,
+                        easing: "linear",
+                    }}
+                />
+            </Entity>
 
-        <Box
-          width={20}
-          height={20}
-          depth={20}
-          material={{
-            // src: "yong_sook.jpg",
-            src: "ari.jpg",
-          }}
-          position={{
-            x: -300,
-            y: 0,
-            z: 0,
-          }}
-          animation={{
-            property: "rotation",
-            to: "360 360 0",
-            loop: 200000,
-            dur: 2000,
-            easing: "linear",
-          }}
-        />
-      </Entity>
-
-      <MyCursor speed={200} />
-    </Scene>
-  );
+            <MyCursor speed={200} />
+        </Scene>
+    );
 }
 
 export { Home as default };
